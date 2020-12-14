0614bf4c1d1d8f9efdf5942b9a00246a1f2ecdcc
DrexHD <nicknamedrex@gmail.com>
2020-12-14 12:51:21 +0100

    disconnect players if they have no time left

--- src/main/java/me/drex/varo/mixin/MixinPlayerManager.java
+++ src/main/java/me/drex/varo/mixin/MixinPlayerManager.java
@@ -1,13 +1,19 @@
 package me.drex.varo.mixin;
 
+import com.mojang.authlib.GameProfile;
 import me.drex.varo.session.SessionManager;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.net.SocketAddress;
 
 @Mixin(PlayerManager.class)
 public class MixinPlayerManager {
@@ -15,6 +21,11 @@ public class MixinPlayerManager {
     @Inject(method = "onPlayerConnect", at = @At(value = "RETURN"))
     public void onPlayerConnect(ClientConnection connection, ServerPlayerEntity player, CallbackInfo info) {
         SessionManager.addSession(player.getUuid());
+    }
+
+    @Inject(method = "checkCanJoin", at = @At(value = "HEAD"))
+    public void blockIfNoPlaytime(SocketAddress address, GameProfile profile, CallbackInfoReturnable<Text> cir) {
+        if (SessionManager.getTimeLeft(profile.getId()) < 0) cir.setReturnValue(new LiteralText("You don't have any time left, wait until tomorrow to rejoin."));
     }
 
 }
